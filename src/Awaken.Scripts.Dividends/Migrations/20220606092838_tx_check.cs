88b618672280d2a44ddc0da0902e2c7a52f0c2aa
weili <woshilw0416@gmail.com>
2022-06-06 17:29:16 +0800

    add migration script

---
+++ src/Awaken.Scripts.Dividends/Migrations/20220606092838_tx_check.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Awaken.Scripts.Dividends.Migrations
+{
+    public partial class tx_check : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TransactionStatus",
+                table: "swap_result",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TransactionType",
+                table: "swap_result",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TransactionStatus",
+                table: "swap_result");
+
+            migrationBuilder.DropColumn(
+                name: "TransactionType",
+                table: "swap_result");
+        }
+    }
+}
