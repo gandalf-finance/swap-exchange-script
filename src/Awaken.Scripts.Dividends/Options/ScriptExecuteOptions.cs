cf4dddf515c74fdd02bad07972306ad06b79e088
weili <woshilw0416@gmail.com>
2022-07-15 19:01:18 +0800

    fix ScriptExecuteOptions

--- src/Awaken.Scripts.Dividends/Options/ScriptExecuteOptions.cs
+++ src/Awaken.Scripts.Dividends/Options/ScriptExecuteOptions.cs
@@ -6,6 +6,6 @@ public class ScriptExecuteOptions
 {
     public string FirstExecutionTime { get; set; } = String.Empty;
     public int FixedTermSeconds { get; set; } = 24 * 60 * 60;
-    public int ExecuteOffsetSeconds = -1;
+    public int ExecuteOffsetSeconds { get; set; } =  -1;
     public bool IsNewReward { get; set; } = false;
 }
